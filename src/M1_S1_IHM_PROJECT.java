61c084783dac79407a58b8a69d2986f38d8f121e
Nicotitine <n.hincelin@laposte.net>
2018-12-05 02:57:43 +0100

    Refractor of the running class to the main folder

--- src/M1_S1_IHM_PROJECT.java
+++ src/M1_S1_IHM_PROJECT.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package m1_s1_ihm_project.View;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -23,12 +23,12 @@ public class M1_S1_IHM_PROJECT extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Database.connect("localhost", 1527, "IHM_Project", "root", "root");
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("MagazinesView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("m1_s1_ihm_project/View/MagazinesView.fxml"));
         Parent root = (Parent)loader.load();
         MagazinesViewController controller = (MagazinesViewController)loader.getController();
         
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("customCss.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("m1_s1_ihm_project/View/customCss.css").toExternalForm());
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.setTitle("Learn English");
