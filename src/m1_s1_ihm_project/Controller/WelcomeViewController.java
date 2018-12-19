d849b0c0da169ec1f68032223ffa31b5e8b7d755
Nicotitine <n.hincelin@laposte.net>
2018-12-19 14:29:52 +0100

    Last few changes

--- src/m1_s1_ihm_project/Controller/WelcomeViewController.java
+++ src/m1_s1_ihm_project/Controller/WelcomeViewController.java
@@ -54,7 +54,7 @@ public class WelcomeViewController implements Initializable {
         VBox mainBox = new VBox();
         VBox secondaryBox = new VBox();
         Text title = new Text("Bienvenu sur Traveler Companion");
-        Text description = new Text("Apprenez rapidement l'anglais avec cet assistant et obtenez un dipome reconnu par l'état.\n\nEt le tout sans débourser un centime !");
+        Text description = new Text("Apprenez rapidement et efficacement l'anglais avec cet assistant !\n\nEt le tout sans débourser un centime !");
         Text toStart = new Text("Pour commencer : ");
         VBox pseudoBox = new VBox();
         Text pseudoLabel = new Text("Entrez un pseudo : (Not working)");
